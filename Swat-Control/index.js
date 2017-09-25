d139e9f5a512399d4ce842b2f1e80cfde1af42d5
JonLuca DeCaro <jdecaro@usc.edu>
2017-09-25 16:04:45 -0700

    more drone work

--- Swat-Control/index.js
+++ Swat-Control/index.js
@@ -1,12 +1,13 @@
-const Drone = require('parrot-minidrone');
-const drone = new Drone({
-    autoconnect: true
-});
+var RollingSpider = require("rolling-spider");
+var rollingSpider = new RollingSpider();
 
-drone.on('connected', () => drone.takeOff());
-drone.on('flightStatusChange', (status) => {
-    if (status === 'hovering') {
-        drone.land();
-        process.exit();
-    }
+// NEW CODE BELOW HERE
+
+rollingSpider.connect(function() {
+    rollingSpider.setup(function() {
+        rollingSpider.flatTrim();
+        rollingSpider.startPing();
+        rollingSpider.flatTrim();
+        console.log('Connected to drone', rollingSpider.name);
+    });
 });
